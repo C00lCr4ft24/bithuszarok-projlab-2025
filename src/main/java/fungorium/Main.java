7e568946f8d3e2a8d7b71caef3b553fecad65601
Adam <bokoradam2004@edu.bme.hu>
2025-02-28 20:12:06 +0100

    while test for Tecton doEffect

--- src/main/java/fungorium/Main.java
+++ src/main/java/fungorium/Main.java
@@ -1,9 +1,11 @@
 package fungorium;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Tecton tec1 = new DeadlyTecton();
-        Tecton tec2 = new DeadlyTecton();
+        Tecton tec2 = new NormalTecton();
 
         tec1.addSpore(new BigSpore());
 
@@ -20,15 +22,42 @@ public class Main {
 
         System.out.println("\n");
 
-        Fungus newFungus = new Fungus();
+        boolean isRunning = true;
 
-        System.out.println("uj fungus amit novesztenenk: " + newFungus);
-        System.out.println("tec1 fungusa: " + tec1.getCurrentFungus());
-        try {
-            System.out.println("trying...");
-            tec1.setCurrentFungus(newFungus);
-            tec1.doEffect();
-        } catch (Exception e) { System.out.println("ilyent nem lehet csinálni"); }
-        System.out.println(tec1.getCurrentFungus());
+        while(isRunning)
+        {
+            Fungus newFungus = new Fungus();
+            System.out.println("\n\nuj fungus amit novesztenenk: " + newFungus);
+            System.out.println("01 tec1: " + tec1.getClass());
+            System.out.println("02 tec2: " + tec2.getClass());
+            System.out.print("Melyik tektonra növesztene: ");
+            Scanner sc = new Scanner(System.in);
+            String input = sc.nextLine();
+            int choice = Integer.parseInt(input);
+            Tecton selectedTecton;
+            switch (choice) {
+                case 1:
+                    selectedTecton = tec1;
+                    break;
+                case 2:
+                    selectedTecton = tec2;
+                    break;
+                default:
+                    selectedTecton = tec1;
+                    break;
+            }
+            System.out.println(selectedTecton + " fungusa: " + selectedTecton.getCurrentFungus());
+            try {
+                System.out.println("gombatestnovesztes probalkozasa...");
+                selectedTecton.setCurrentFungus(newFungus);
+                selectedTecton.doEffect();
+            } catch (Exception e) { System.out.println("ilyent nem lehet csinálni"); }
+            System.out.println(selectedTecton + " fungusa: " + selectedTecton.getCurrentFungus());
+        }
+
+
+
+
+
     }
 }
