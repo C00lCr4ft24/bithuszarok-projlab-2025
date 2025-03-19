2726ad2683010e14015a10f02529d21bcc3a674b
Karácsony Gábor <karacsony.g@hotmail.com>
2025-03-19 22:49:54 +0100

    FungoriumEntity gameStep function.

--- src/main/java/fungorium/FungoriumEntity.java
+++ src/main/java/fungorium/FungoriumEntity.java
@@ -1,4 +1,8 @@
 package fungorium;
 
 public interface FungoriumEntity {
+    /**
+     * With this function, the game elements will sense the passage of time and perform the necessary actions.
+     */
+    public void gameStep();
 }
