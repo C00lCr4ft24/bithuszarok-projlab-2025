b768074f1562d7d0cc4e117f7c07c1fe61501f6a
Karácsony Gábor <karacsony.g@hotmail.com>
2025-03-23 22:08:15 +0100

    Fungus now has a position.

--- src/main/java/fungorium/mycelium/Fungus.java
+++ src/main/java/fungorium/mycelium/Fungus.java
@@ -5,10 +5,16 @@ import fungorium.tecton.Tecton;
 
 public class Fungus implements FungoriumEntity {
 
+    private MyceliumJunction junctionPosition;
     private int fungusLevel;
     private int sporeLevel;
 
     public Fungus() { System.out.println("New Fungus created: " + this); }
+
+    public Fungus(MyceliumJunction junctionPosition) {
+        this();
+        this.junctionPosition = junctionPosition;
+    }
 
     public void spreadSpores(Tecton target) { printAction("spreadSpores"); }
 
