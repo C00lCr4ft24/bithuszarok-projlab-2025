1d70b664f25bc8b5f15bb889c191659497a3fc9f
Adam <bokoradam2004@edu.bme.hu>
2025-03-23 08:00:44 +0100

    Insect methods defined

--- src/main/java/fungorium/Insect.java
+++ src/main/java/fungorium/Insect.java
@@ -1,4 +1,36 @@
 package fungorium;
+import fungorium.mycelium.MyceliumConnection;
+import fungorium.spore.Spore;
+import fungorium.tecton.Tecton;
 
-public class Insect {
+public class Insect implements FungoriumEntity {
+
+    private enum Speed { SLOW, MEDIUM, FAST }
+
+    private int     eatenNutrient;
+    private Speed   speed;
+    private boolean canCutMycelium;
+    private boolean isStunned;
+
+    private void resetEffectValues() {  }
+
+    public Insect() {
+        eatenNutrient = 0;
+        speed = Speed.MEDIUM;
+        canCutMycelium = true;
+        isStunned = false;
+    }
+
+    public void cutMyceliumConnection(MyceliumConnection mc) { printAction("cutMyceliumConnection"); }
+    public void eatSpore(Spore spore)                        { printAction("eatSpore");              }
+    public void move(Tecton target)                          { printAction("move");                  }
+    public void setStunned()                                 { printAction("setStunned");            }
+    public void blockMyceliumCut()                           { printAction("blockMyceliumCut");      }
+    public void increaseSpeed()                              { printAction("increaseSpeed");         }
+    public void decreaseSpeed()                              { printAction("decreaseSpeed");         }
+
+    @Override
+    public void gameStep() { System.out.println("gameStep");  }
+
+    private void printAction(String methodName) { System.out.println(methodName + " called on " + this); }
 }
