c625395b6423714343a3667641fa42711e7f26cb
blahameki <czkormi@gmail.com>
2025-03-23 22:22:02 +0100

    spore repo javadocs

--- src/main/java/fungorium/spore/Spore.java
+++ src/main/java/fungorium/spore/Spore.java
@@ -3,20 +3,53 @@ package fungorium.spore;
 import fungorium.FungoriumEntity;
 import fungorium.Insect;
 
+package fungorium.spore;
+
+import fungorium.FungoriumEntity;
+import fungorium.Insect;
+
+/**
+ * Egy absztrakt spórát reprezentáló osztály, amely létrejöhet és kilövődhet, majd egy
+ * Tecton-on elhelyezkedve elfogyasztható vagy felhasználható.
+ */
 abstract public class Spore implements FungoriumEntity {
 
+    /**
+     * A spórához tartozó tápanyagtartalom.
+     */
     private final int nutrient;
+
+    /**
+     * A spóra hatásának időtartama.
+     */
     private final int effectTime;
 
+    /**
+     * Létrehoz egy új spóra példányt a megadott tápanyag- és hatásidő értékekkel.
+     *
+     * @param nutrient A spóra tápanyagtartalma.
+     * @param effectTime A spóra hatásának időtartama.
+     */
     public Spore(int nutrient, int effectTime) {
         this.nutrient = nutrient;
         this.effectTime = effectTime;
     }
 
+    /**
+     * Visszaadja a spóra tápanyagtartalmát.
+     *
+     * @return A spóra tápanyagtartalma.
+     */
     public int getNutrientValue()
     {
         printAction("getNutrientValue");
         return this.nutrient;
     }
+
+    /**
+     * Absztrakt metódus, amely kifejti a spóra egyedi hatását a megadott rovarra.
+     *
+     * @param insect A {@link Insect}, amelyre a spóra hatással van.
+     */
     public abstract void doEffect(Insect insect);
 }
