a117ede4a7c08255f0df717e375c260a1e5b1160
blahameki <czkormi@gmail.com>
2025-03-23 22:09:21 +0100

    AntiCutSpore.java, Fungus.java, MyceliumConnection.java, MyceliumJunction.java javadocs

--- src/main/java/fungorium/mycelium/Fungus.java
+++ src/main/java/fungorium/mycelium/Fungus.java
@@ -3,15 +3,36 @@ package fungorium.mycelium;
 import fungorium.FungoriumEntity;
 import fungorium.tecton.Tecton;
 
+/**
+ * Egy gomba entitást reprezentál, amely képes spórákat szórni és fejlődni az idő múlásával.
+ */
 public class Fungus implements FungoriumEntity {
 
+    /**
+     * A gomba fejlődési szintjét reprezentálja.
+     */
     private int fungusLevel;
+
+    /**
+     * A gomba spóraszintjét reprezentálja. Ez szükséges a spórák szétszórásához.
+     */
     private int sporeLevel;
 
+    /**
+     * Egy új `Fungus` példányt hoz létre.
+     */
     public Fungus() { System.out.println("New Fungus created: " + this); }
 
+    /**
+     * Spórákat szór szét a megadott cél Tectonra.
+     *
+     * @param target A {@link Tecton}, amelyre a spórák kerülnek szétszórásra.
+     */
     public void spreadSpores(Tecton target) { printAction("spreadSpores"); }
 
+    /**
+     * Végrehajtja a következő játék lépést. Növeli a gomba spóraszintjét.
+     */
     @Override
     public void gameStep() { printAction("gameStep"); }
 }
