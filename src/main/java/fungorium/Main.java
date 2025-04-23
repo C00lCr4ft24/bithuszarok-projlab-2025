8df7723e0f64900c90c62c4305079e4ed973082a
Karácsony Gábor <karacsony.g@hotmail.com>
2025-04-23 21:58:12 +0200

    Ideiglenesen visszakapcsolt Skeleton osztály.

--- src/main/java/fungorium/Main.java
+++ src/main/java/fungorium/Main.java
@@ -2,6 +2,8 @@ package fungorium;
 
 public class Main {
     public static void main(String[] args) {
-
+        // ideiglenesen kapcsoltam csak vissza
+        var skeleton = new Skeleton();
+        skeleton.start();
     }
 }
