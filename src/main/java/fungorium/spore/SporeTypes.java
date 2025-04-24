93421067dff7ab0eb2a0102cb9a700d37780930b
Karácsony Gábor <karacsony.g@hotmail.com>
2025-04-24 23:25:42 +0200

    Egy külön enum osztály, melyben a különböző létrehozható Spóra típusok vannak + Random. Szükséges a Fungus Spóra lövésekor, hogy ne mindig random legyen az új Spóra típusa, hanem megadott.

---
+++ src/main/java/fungorium/spore/SporeTypes.java
@@ -0,0 +1,13 @@
+package fungorium.spore;
+
+/**
+ * Különböző létrehozható spórák típusai
+ */
+public enum SporeTypes {
+    ANTI_CUT_SPORE,
+    REPLICATION_SPORE,
+    SLOW_DOWN_SPORE,
+    SPEED_UP_SPORE,
+    STUN_SPORE,
+    RANDOM
+}
