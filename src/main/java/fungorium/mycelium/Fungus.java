1dd36177ff210f90f82e924c67dfbe08afbe1d24
Karácsony Gábor <karacsony.g@hotmail.com>
2025-04-24 23:36:51 +0200

    Fungus osztály alap implementációja.
    spreadSpores függvényben, ha meg van adva az új Spóra típusa (nem Random), akkor a létrehozott spóra tápanyagtartalma: 200 és a hatásideje: 3 lesz.

--- src/main/java/fungorium/mycelium/Fungus.java
+++ src/main/java/fungorium/mycelium/Fungus.java
@@ -1,7 +1,7 @@
 package fungorium.mycelium;
 
 import fungorium.FungoriumEntity;
-import fungorium.spore.SporeFactory;
+import fungorium.spore.*;
 import fungorium.tecton.Tecton;
 
 /**
@@ -10,26 +10,34 @@ import fungorium.tecton.Tecton;
 public class Fungus implements FungoriumEntity {
 
     /**
+     * A spóra lövéshez szükséges minimum spóraszint.
+     */
+    private static final int MINIMUM_SPORE_LEVEL_TO_SPREAD_SPORE = 5;
+    /**
      * A gombához tartozó MyceliumJunction pozíció.
      */
     private MyceliumJunction junctionPosition;
     /**
      * A gomba fejlődési szintjét reprezentálja.
      */
-    private int fungusLevel;
-
+    private FungusLevel fungusLevel;
     /**
      * A gomba spóraszintjét reprezentálja. Ez szükséges a spórák szétszórásához.
      */
     private int sporeLevel;
+    /**
+     * Tárolja, hogy tud-e már spórát lőni a gombatest.
+     */
+    private boolean canSpreadSpore;
 
     /**
      * Egy új `Fungus` példányt hoz létre.
      */
-    public Fungus() { 
+    public Fungus() {
         System.out.println("New Fungus created: " + this);
-        sporeLevel  = 0;
-        fungusLevel = 1;
+        sporeLevel = 0;
+        fungusLevel = FungusLevel.SMALL;
+        canSpreadSpore = false;
     }
 
     /**
@@ -43,20 +51,68 @@ public class Fungus implements FungoriumEntity {
     }
 
     /**
-     * Spórákat szór szét a megadott cél Tectonra.
+     * Spórát szór szét a megadott cél Tectonra, ha a hatótávolságán belül van. Öregíti a következő fázisra a szintjét.
      *
-     * @param target A {@link Tecton}, amelyre a spórák kerülnek szétszórásra.
+     * @param target    A {@link Tecton}, amelyre a spórák kerülnek szétszórásra.
+     * @param sporeType Megmondja, hogy milyen típusú spórát lőjön ki a Fungus. SporeTypes enum-al mondja meg.
+     * @return True, ha elhalt a gombatest, egyébként False.
      */
-    public void spreadSpores(Tecton target) { 
-        printAction("spreadSpores"); 
-        for(int i = 1; i <= sporeLevel; ++i) {
-            target.putASpore(SporeFactory.createSpore());
+    public boolean spreadSpores(Tecton target, SporeTypes sporeType) {
+        printAction("spreadSpores");
+        boolean fungusHasDied = false;
+        if (canSpreadSpore) {
+            boolean targetInDistance = false;
+            switch (fungusLevel) {
+                case SMALL, MEDIUM ->
+                        targetInDistance = junctionPosition.getPosition().isThisYourNeighbourInRange(target, 1);
+                case BIG, LARGE ->
+                        targetInDistance = junctionPosition.getPosition().isThisYourNeighbourInRange(target, 2);
+            }
+            if (targetInDistance) {
+                switch (fungusLevel) {
+                    case SMALL -> fungusLevel = FungusLevel.MEDIUM;
+                    case MEDIUM -> fungusLevel = FungusLevel.BIG;
+                    case BIG -> fungusLevel = FungusLevel.LARGE;
+                    case LARGE -> fungusHasDied = true;
+                }
+                Spore newSpore = null;
+                switch (sporeType) {
+                    case ANTI_CUT_SPORE -> newSpore = new AntiCutSpore(200, 3);
+                    case REPLICATION_SPORE -> newSpore = new ReplicationSpore(200, 3);
+                    case SLOW_DOWN_SPORE -> newSpore = new SlowDownSpore(200, 3);
+                    case SPEED_UP_SPORE -> newSpore = new SpeedUpSpore(200, 3);
+                    case STUN_SPORE -> newSpore = new StunSpore(200, 3);
+                    case RANDOM -> newSpore = SporeFactory.createSpore();
+                }
+                junctionPosition.getPosition().putASpore(newSpore);
+                sporeLevel = 0;
+                canSpreadSpore = false;
+            }
+            if (fungusHasDied) {
+                junctionPosition.removeFungus();
+                junctionPosition = null;
+            }
         }
+        return fungusHasDied;
     }
 
     /**
      * Végrehajtja a következő játék lépést. Növeli a gomba spóraszintjét.
      */
     @Override
-    public void gameStep() { printAction("gameStep"); }
+    public void gameStep() {
+        printAction("gameStep");
+        if (!canSpreadSpore) {
+            sporeLevel++;
+            if (sporeLevel >= MINIMUM_SPORE_LEVEL_TO_SPREAD_SPORE) {
+                canSpreadSpore = true;
+            }
+        }
+    }
+
+    /**
+     * A gombatest különböző növekvési szintjei/állapotai.
+     * Az első kettőben egy Tecton távolságra tud lőni, míg a másik kettőben már két Tecton távolságra is tud.
+     */
+    private enum FungusLevel {SMALL, MEDIUM, BIG, LARGE}
 }
