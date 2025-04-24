cad27b875e8f7d1fc3e5abe7848bcafe10dc70e3
Karácsony Gábor <karacsony.g@hotmail.com>
2025-04-24 21:27:48 +0200

    PreserverTecton osztály konstruktorai.

--- src/main/java/fungorium/tecton/PreserverTecton.java
+++ src/main/java/fungorium/tecton/PreserverTecton.java
@@ -3,6 +3,22 @@ package fungorium.tecton;
 public class PreserverTecton extends Tecton {
 
     /**
+     * Létrehoz egy új PreserverTecton példányt a megadott MyceliumJunction-ok listájával.
+     *
+     * @param t A Tecton szomszédjai
+     */
+    public PreserverTecton(ArrayList<Tecton> t) {
+        super(t);
+    }
+
+    /**
+     * Létrehoz egy új, üres PreserverTecton példányt.
+     */
+    public PreserverTecton() {
+        super();
+    }
+
+    /**
      * A saját osztály típusából csinál egy új alappéldányt és visszaadja azt.
      *
      * @return Az példány, melynek megyezik az osztály típusa azzal, akin hívódik.
