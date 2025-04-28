8b2442c48f3891eaaa5f7b70a320cb9d48e375a5
Benő Levente <benolevi@gmail.com>
2025-04-28 18:17:57 +0200

    Javadoc kommentek a TestFramework osztályhoz

--- src/main/java/fungorium/TestFramework.java
+++ src/main/java/fungorium/TestFramework.java
@@ -1,16 +1,31 @@
 package fungorium;
 
 public class TestFramework {
+    
+    /**
+     * Statikus logger string, az elvárt teszteredmény és az akutális teszteredmény logger alapú összehasonlításához
+     */
     private static String currentLog = new String();
 
+    /**
+     * Visszaadja a futás során keletkezett log üzenetet.
+     * @return A futás során feljegyzett események
+     */
     public static String getCurrentLog() {
         return currentLog;
     }
     
+    /**
+     * Hozzáfűzi a loghoz az adott függvény eseményéhez tartozó stringet.
+     * @param log az adott esemény/függvény üzenete
+     */
     public static void logOutput(String log) {
         currentLog.concat(log);
     }
 
+    /**
+     * Üríti a futás során keletkezett logot, és újat kezd.
+     */
     public static void clearLog() {
         currentLog = new String();
     }
