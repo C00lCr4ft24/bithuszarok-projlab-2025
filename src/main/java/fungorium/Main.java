184e40e82e1b217b2d10ae8a354e18d3f19dc3b1
Adam <bokoradam2004@edu.bme.hu>
2025-05-11 09:29:42 +0200

    Optimalizált MainFrame hívás

--- src/main/java/fungorium/Main.java
+++ src/main/java/fungorium/Main.java
@@ -2,9 +2,11 @@ package fungorium;
 
 import fungorium.view.frames.MainFrame;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        MainFrame frame = new MainFrame();
+        SwingUtilities.invokeLater(MainFrame::new);
         //GameModel game = new GameModel();
         //TestFramework.testMenu(game);
         //TestFramework.runTest("test_1", game);
