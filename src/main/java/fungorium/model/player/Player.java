c4ebd66cc7610aa5b9e24700e55d520e8920cc6f
Adam <bokoradam2004@edu.bme.hu>
2025-05-13 09:30:55 +0200

    Player osztály bővítése

--- src/main/java/fungorium/model/player/Player.java
+++ src/main/java/fungorium/model/player/Player.java
@@ -1,8 +1,15 @@
 package fungorium.model.player;
 
 public class Player {
-    private PlayerTypes type;
-    public Player(PlayerTypes type) {
+    private final PlayerTypes type;
+    private final String name;
+    public Player(PlayerTypes type, String name) {
         this.type = type;
+        this.name = name;
     }
+
+    public PlayerTypes getType() { return type; }
+
+    @Override
+    public String toString() { return name; }
 }
