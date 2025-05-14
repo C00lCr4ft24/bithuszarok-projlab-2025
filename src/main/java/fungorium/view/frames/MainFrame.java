f6fde0b0eb62de93e128b052127f5413a0e56896
Adam <bokoradam2004@edu.bme.hu>
2025-05-14 08:15:23 +0200

    MainFrame-be került a GameModel konstruktorhívás

--- src/main/java/fungorium/view/frames/MainFrame.java
+++ src/main/java/fungorium/view/frames/MainFrame.java
@@ -4,23 +4,26 @@ import java.awt.BorderLayout;
 
 import javax.swing.JFrame;
 
+import fungorium.GameModel;
 import fungorium.view.bars.FungoriumMenuBar;
 import fungorium.view.bars.FungusPlayerToolBar;
 import fungorium.view.bars.GameSettingsToolBar;
 import fungorium.view.bars.InsectPlayerToolBar;
-import fungorium.view.buttons.NewGameButton;
 
 public class MainFrame extends JFrame {
     public MainFrame() {
+
+        GameModel gameModel = new GameModel();
+
         this.setJMenuBar(new FungoriumMenuBar());
         this.setLayout(new BorderLayout());
         
         this.add(new InsectPlayerToolBar(), BorderLayout.WEST);
         this.add(new FungusPlayerToolBar(), BorderLayout.EAST);
         this.add(new GameSettingsToolBar(), BorderLayout.SOUTH);
-        this.setSize(1280, 720);
+        this.setSize(720, 720);
         this.setLocationRelativeTo(null);
-        this.setExtendedState(MAXIMIZED_BOTH);
+        //this.setExtendedState(MAXIMIZED_BOTH);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setVisible(true);
     }
