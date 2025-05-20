081ca14f40c754c08842aec8036ec137500d0aa5
Benő Levente <benolevi@gmail.com>
2025-05-20 17:07:54 +0200

    Ablakok neve beállítva

--- src/main/java/fungorium/view/frames/HelperFrame.java
+++ src/main/java/fungorium/view/frames/HelperFrame.java
@@ -5,8 +5,11 @@ import javax.swing.JTextArea;
 
 public class HelperFrame extends JFrame {
     private static final String rulesDescription = "Szabályok";
+    
     public HelperFrame() {
         this.add(new JTextArea(rulesDescription));
+
+        this.setTitle("Súgó");
         this.setSize(480,640);
         this.setLocationRelativeTo(null);
         this.setVisible(false);
