7f6cf9b070525da35b2a305e9765e44e1050fa46
Karácsony Gábor <karacsony.g@hotmail.com>
2025-05-21 00:21:36 +0200

    CutMyceliumToolBar alap szövege és ComboBoxa.

--- src/main/java/fungorium/view/toolbars/CutMyceliumToolBar.java
+++ src/main/java/fungorium/view/toolbars/CutMyceliumToolBar.java
@@ -1,6 +1,8 @@
 package fungorium.view.toolbars;
 
 import fungorium.controller.Controller;
+import fungorium.model.Insect;
+import fungorium.model.tecton.Tecton;
 import fungorium.view.buttons.ButtonFactory;
 
 import javax.swing.JComboBox;
@@ -8,10 +10,14 @@ import javax.swing.JLabel;
 import javax.swing.JToolBar;
 
 public class CutMyceliumToolBar extends JToolBar {
+    public static final JComboBox<Insect> availableInsectsComboBox = new JComboBox<>();
+    public static final JComboBox<Tecton> availableConnectionsComboBox = new JComboBox<>();
 
     public CutMyceliumToolBar() {
         this.add(new JLabel("Válassz egy rovart: "));
-        this.add(new JComboBox<>());
+        this.add(availableInsectsComboBox);
+        this.add(new JLabel(" Válassz egy elvágandó fonalat: "));
+        this.add(availableConnectionsComboBox);
         this.add(ButtonFactory.getNewButton("Elfogad", Controller::cutMyceliumConfirmPressed));
 
         this.setFloatable(false);
