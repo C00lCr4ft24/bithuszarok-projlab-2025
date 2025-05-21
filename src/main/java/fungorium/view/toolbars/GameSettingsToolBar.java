3939329a9c3a7c927d71f8fda58ecd20ddd4d5df
Karácsony Gábor <karacsony.g@hotmail.com>
2025-05-21 03:37:13 +0200

    Alapértelmezetten sik gomb ki van kapcsolva.

--- src/main/java/fungorium/view/toolbars/GameSettingsToolBar.java
+++ src/main/java/fungorium/view/toolbars/GameSettingsToolBar.java
@@ -43,6 +43,7 @@ public class GameSettingsToolBar extends JToolBar {
         add(playerNameTextField);
 
         add(skipStepButton);
+        skipStepButton.setEnabled(false);
 
         JLabel insectText = new JLabel("Rovarászok Száma: ");
         insectText.setFocusable(false);
